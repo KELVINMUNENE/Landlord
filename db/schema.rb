a7aef299d3bccf9951d3c40ca805056513734e52
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-29 07:30:32 +0300

    created migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829042451) do
+ActiveRecord::Schema.define(version: 20170829042807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,24 @@ ActiveRecord::Schema.define(version: 20170829042451) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "emergencies", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "maintanances", force: :cascade do |t|
     t.string "name"
     t.string "area"
     t.string "problem"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
