4bd9c12cf6f634ae44c6b33443564faf62149c19
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-29 07:15:00 +0300

    created columns for emergrncy

--- db/migrate/20170829041253_create_emergencys.rb
+++ db/migrate/20170829041253_create_emergencys.rb
@@ -1,6 +1,11 @@
 class CreateEmergencys < ActiveRecord::Migration[5.1]
   def change
     create_table :emergencys do |t|
+    	t.column :title, :string
+    	t.column :description, :string
+
+
+    	t.timestamp
     end
   end
 end
