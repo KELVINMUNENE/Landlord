2b805cf11ca360a8139ba8356d3c28a403c06fa2
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-29 08:23:48 +0300

    permitted title and description

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -69,6 +69,6 @@ class EmergenciesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def emergency_params
-      params.fetch(:emergency, {})
+      params.fetch(:emergency).permit(:title, :description, :created_at)
     end
 end
