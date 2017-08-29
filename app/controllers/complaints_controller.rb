484882d15483e45d33837665d893785aa5299450
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-29 07:55:46 +0300

    permitted attributes to display maintanance

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -69,7 +69,7 @@ class ComplaintsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def complaint_params
-      params.fetch(:complaint).permit(:name,:plot,:message)
+      params.fetch(:complaint).permit(:name,:plot,:message, :created_at)
     end
 end
 
--- app/controllers/maintanances_controller.rb
+++ app/controllers/maintanances_controller.rb
@@ -69,6 +69,6 @@ class MaintanancesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def maintanance_params
-      params.fetch(:maintanance, {})
+      params.fetch(:maintanance).permit(:name, :area, :problem, :timestamp)
     end
 end
