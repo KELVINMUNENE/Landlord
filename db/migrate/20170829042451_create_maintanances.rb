09dc14a2f85f0bcd49ca767090d2029889dbbe9b
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-29 07:26:58 +0300

    created a migration

--- db/migrate/20170829042451_create_maintanances.rb
+++ db/migrate/20170829042451_create_maintanances.rb
@@ -1,6 +1,9 @@
 class CreateMaintanances < ActiveRecord::Migration[5.1]
   def change
     create_table :maintanances do |t|
+    	t.column :name, :string
+    	t.column :area, :string
+    	t.column :problem, :string
 
       t.timestamps
     end
