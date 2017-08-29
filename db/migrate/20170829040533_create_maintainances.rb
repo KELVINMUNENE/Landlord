430d59d4b38ce6c8e7a5fd4aeb53dcc1806f16c1
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-29 07:08:34 +0300

    created maintanance columns

--- db/migrate/20170829040533_create_maintainances.rb
+++ db/migrate/20170829040533_create_maintainances.rb
@@ -1,6 +1,9 @@
 class CreateMaintainances < ActiveRecord::Migration[5.1]
   def change
     create_table :maintainances do |t|
+    	t.column :name, :string
+    	t.column :area, :string
+    	t.column :problem, :string
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829040117) do
+ActiveRecord::Schema.define(version: 20170829040533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20170829040117) do
     t.string "name"
     t.string "plot"
     t.string "message"
+  end
+
+  create_table "maintainances", force: :cascade do |t|
+    t.string "name"
+    t.string "area"
+    t.string "problem"
   end
 
 end
