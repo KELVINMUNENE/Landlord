f53c07dee8d67dc225d05f5bb84fe94147fa7f9c
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-29 07:03:31 +0300

    made columns for three attributes

--- db/migrate/20170829040117_create_complaints.rb
+++ db/migrate/20170829040117_create_complaints.rb
@@ -1,6 +1,11 @@
 class CreateComplaints < ActiveRecord::Migration[5.1]
   def change
     create_table :complaints do |t|
+    	t.column :name, :string
+    	t.column :plot, :string
+    	t.column :message, :string
+
+    	t.timestamp
     end
   end
 end
