1ade5ea575c399e7ffdba6f976800970bb48f03e
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-29 08:15:24 +0300

    permitted notifications attributes

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -69,6 +69,7 @@ class NotificationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def notification_params
-      params.fetch(:notification, {})
+      params.fetch(:notification).permit(:title, :description, :created_at)
     end
 end
+
