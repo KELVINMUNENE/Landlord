42c4b5b97a383980876c0ec733b7f69b150559bc
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-29 07:11:56 +0300

    created columns for title and description

--- db/migrate/20170829040909_create_notifications.rb
+++ db/migrate/20170829040909_create_notifications.rb
@@ -1,6 +1,11 @@
 class CreateNotifications < ActiveRecord::Migration[5.1]
   def change
     create_table :notifications do |t|
+    	t.column :title, :string
+    	t.column :description, :string
+
+
+    	t.timestamp
     end
   end
 end
