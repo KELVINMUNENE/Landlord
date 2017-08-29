bee4a8133c7db0b5de1ee6f7c6c573de1b5accf3
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-29 07:30:00 +0300

    gave my notifications and emergency pages columns

--- db/migrate/20170829042720_create_notifications.rb
+++ db/migrate/20170829042720_create_notifications.rb
@@ -1,6 +1,8 @@
 class CreateNotifications < ActiveRecord::Migration[5.1]
   def change
     create_table :notifications do |t|
+    	t.column :title, :string
+    	t.column :description, :string
 
       t.timestamps
     end
--- db/migrate/20170829042807_create_emergencies.rb
+++ db/migrate/20170829042807_create_emergencies.rb
@@ -1,6 +1,9 @@
 class CreateEmergencies < ActiveRecord::Migration[5.1]
   def change
     create_table :emergencies do |t|
+    	t.column :title, :string
+    	t.column :description, :string
+
 
       t.timestamps
     end
