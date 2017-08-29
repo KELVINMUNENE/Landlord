29b673b919f5a8d3547db20e259834efc5e59148
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-29 08:45:41 +0300

    added columns

--- db/migrate/20170829053620_create_saccos.rb
+++ db/migrate/20170829053620_create_saccos.rb
@@ -1,6 +1,10 @@
 class CreateSaccos < ActiveRecord::Migration[5.1]
   def change
     create_table :saccos do |t|
+    	t.column :name, :string
+    	t.column :Id, :integer
+    	t.column :house, :string
+    	t.column :amount, :money
     end
   end
 end
